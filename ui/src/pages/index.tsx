5a19f3a8b7929d62c01ec8a4e1cc4e042e84f202
Sadanand Pai M <sadanand.pai@NTPL-IT-MAC-LAP0455.local>
2023-04-13 20:24:10 +0530

    UI init

---
+++ ui/src/pages/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+      <h1 className="text-2xl">Self Governance DAO tooling</h1>
+    </main>
+  );
+}
