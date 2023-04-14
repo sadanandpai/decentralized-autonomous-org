3132a52e6ad031d090ac3310116ce2627e76eb33
Sadanand Pai M <sadanand.pai@NTPL-IT-MAC-LAP0455.local>
2023-04-14 10:24:05 +0530

    Chakra UI integration

--- ui/src/pages/index.tsx
+++ ui/src/pages/index.tsx
@@ -1,7 +1,12 @@
+import { Button, ButtonGroup } from "@chakra-ui/react";
+
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-4">
+    <main className="flex min-h-screen flex-col items-center p-4">
       <h1 className="text-2xl">Self Governance DAO tooling</h1>
+      <Button variant="solid" colorScheme="green">
+        Register
+      </Button>
     </main>
   );
 }
