ac8d6c3e4abfdac763620a603bbd59422517fd9f
Anand Kumar <anand.kumar@NTPL-IT-MACLAP0370.local>
2023-04-18 18:38:19 +0530

    proposal contract createProposal testcases

--- backend/test/Proposal.js
+++ backend/test/Proposal.js
@@ -1,16 +1,71 @@
-const { time, loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { expect } = require('chai');
+// Import the necessary modules
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
 
-describe('Proposal', function () {
-  async function deployProposalFixture() {
-    const daoUser = await ethers.getContractFactory('DaoUser');
-    const daoUserContract = await daoUser.deploy();
-    const Proposal = await ethers.getContractFactory('Proposal');
-    return await Proposal.deploy(daoUserContract.address);
-  }
+describe("Proposal Contract", function () {
+  let daoUser;
+  let proposal;
+  let owner, addr1, addr2;
+  // Deploy the contracts before each test
+  beforeEach(async function () {
+    // Deploy DaoUser contract
+    const DaoUser = await ethers.getContractFactory("DaoUser");
+    daoUser = await DaoUser.deploy();
 
-  it('should get deployed', async function () {
-    const proposal = await loadFixture(deployProposalFixture);
-    expect(proposal).not.to.be.undefined;
+    // Deploy Proposal contract
+    const Proposal = await ethers.getContractFactory("Proposal");
+    proposal = await Proposal.deploy(daoUser.address);
+    await proposal.deployed();
+
+    [owner, addr1, addr2] = await ethers.getSigners();
+  });
+
+  it("Should create a new proposal", async function () {
+    //Register user
+    await daoUser.addUserDetails('Test', 'User', 'test@gmail.com', 'pic');
+
+    // Call createProposal function to create a new proposal
+    const title = 'Test Proposal';
+    const description = 'Test proposal description';
+    const uploadDocPath = 'https://test-doc-path';
+    const tx = await proposal.createProposal(title, description, uploadDocPath);
+
+    // Check the emitted event
+    const receipt = await tx.wait();
+    expect(receipt.events[0].event).to.equal('NewProposalCreated');
+    expect(receipt.events[0].args.proposalId).to.equal(0);
+
+    // Check that the proposal was created successfully
+    const proposals = await proposal.getAllProposals();
+    expect(proposals[0].owner).to.equal(await ethers.provider.getSigner(0).getAddress());
+    expect(proposals[0].title).to.equal(title);
+    expect(proposals[0].description).to.equal(description);
+    expect(proposals[0].uploadDocPath).to.equal(uploadDocPath);
+    expect(proposals[0].status).to.equal(0);
+    expect(proposals[0].id).to.equal(0);
+  });
+
+  it('should not allow creation of proposal when another is active', async function () {
+    //Register user
+    await daoUser.addUserDetails('Test', 'User', 'test@gmail.com', 'pic');
+
+    const title = 'Test Proposal';
+    const description = 'Test proposal description';
+    const uploadDocPath = 'https://test-doc-path';
+    await proposal.createProposal(title, description, uploadDocPath);
+
+    await expect(proposal.createProposal(title, description, uploadDocPath)).to.be.revertedWith(
+      'Already another Proposal is active'
+    );
+  });
+
+  it('should not allow creation of proposal by unregistered user', async function () {
+    const title = 'Test Proposal';
+    const description = 'Test proposal description';
+    const uploadDocPath = 'https://test-doc-path';
+
+    await expect(proposal.createProposal(title, description, uploadDocPath)).to.be.revertedWith(
+      'User Not registered'
+    );
   });
 });
