6e169a68df39944773f76633f14b9e83a7423a67
Anand Kumar <anand.kumar@NTPL-IT-MACLAP0370.local>
2023-04-19 17:26:40 +0530

    updated test cases

--- backend/test/DaoUser.js
+++ backend/test/DaoUser.js
@@ -3,71 +3,178 @@ const { expect } = require('chai');
 
 describe('User', function () {
   let daoUser;
-  async function deployUserFixture() {
-    const daoUser = await ethers.getContractFactory('DaoUser');
-    return await daoUser.deploy();
-  }
-
-  it('should get deployed', async function () {
-    daoUser = await loadFixture(deployUserFixture);
-    expect(daoUser).not.to.be.undefined;
-  });
-
-  beforeEach(async function () {
-    const DaoUser = await ethers.getContractFactory('DaoUser');
-    daoUser = await DaoUser.deploy();
-    await daoUser.deployed();
-  });
 
   describe('addUserDetails', function () {
+    beforeEach(async function () {
+      const DaoUser = await ethers.getContractFactory('DaoUser');
+      daoUser = await DaoUser.deploy();
+      await daoUser.deployed();
+    });
+
+    it('should get deployed', async function () {
+      expect(daoUser).not.to.be.undefined;
+    });
+
+    it("should revert if any field is missing", async function () {
+      await expect(
+        daoUser.addUserDetails("", "Doe", "john.doe@example.com", "https://example.com/john", {
+          value: ethers.utils.parseEther("0.1"),
+        })
+      ).to.be.revertedWith("First name is required");
+
+      await expect(
+        daoUser.addUserDetails("John", "", "john.doe@example.com", "https://example.com/john", {
+          value: ethers.utils.parseEther("0.1"),
+        })
+      ).to.be.revertedWith("Last name is required");
+
+      await expect(
+        daoUser.addUserDetails("John", "Doe", "", "https://example.com/john", {
+          value: ethers.utils.parseEther("0.1"),
+        })
+      ).to.be.revertedWith("Email is required");
+
+      await expect(
+        daoUser.addUserDetails("John", "Doe", "john.doe@example.com", "", {
+          value: ethers.utils.parseEther("0.1"),
+        })
+      ).to.be.revertedWith("Pic is required");
+
+      await expect(
+        daoUser.addUserDetails("J".repeat(51), "Doe", "john.doe@example.com", "https://example.com/john", {
+          value: ethers.utils.parseEther("0.1"),
+        })
+      ).to.be.revertedWith("First name must be less than or equal to 50 characters");
+    });
+
     it('should add a new user', async function () {
       const firstName = 'John';
       const lastName = 'Doe';
       const email = 'johndoe@example.com';
       const pic = 'imagepath';
-      await daoUser.addUserDetails(firstName, lastName, email, pic);
+      //call add user details function with valid input parameters and send 0.1 ether
+      const tx = await daoUser.addUserDetails(firstName, lastName, email, pic, { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+      
+      // wait for the transaction to be mined
+      await tx.wait();
+
+      // check that the user's details were added correctly
       const user = await daoUser.userDetails(await ethers.provider.getSigner(0).getAddress());
       expect(user.userAddress).to.equal(await ethers.provider.getSigner(0).getAddress());
       expect(user.firstName).to.equal(firstName);
       expect(user.lastName).to.equal(lastName);
       expect(user.email).to.equal(email);
       expect(user.pic).to.equal(pic);
+
+      // check that the user count has increased by 1
+      expect(await daoUser.userCount()).to.equal(1);
     });
 
     it('should not add a new user with the same address', async function () {
-      const firstName1 = 'John';
-      const lastName1 = 'Doe';
-      const email1 = 'johndoe@example.com';
-      const pic1 = 'imagepath';
-      const firstName2 = 'Jane';
-      const lastName2 = 'Doe';
-      const email2 = 'janedoe@example.com';
-      const pic2 = 'imagepath';
-      await daoUser.addUserDetails(firstName1, lastName1, email1, pic1);
-      await expect(daoUser.addUserDetails(firstName2, lastName2, email2, pic2)).to.be.revertedWith(
+
+      await daoUser.addUserDetails('John', 'Doe', 'johndoe@example.com', 'imagepath', { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+      await expect(daoUser.addUserDetails('Jane', 'Doe', 'janedoe@example.com', 'imagepath', { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 })).to.be.revertedWith(
         'User already exists'
       );
+      
+    });
+
+    it("should not add a user without sending 0.1 ether", async function() {
+      // try to add a user with valid input parameters but without sending 0.1 ether
+      await expect(daoUser.addUserDetails("John", "Doe", "johndoe@example.com", "pic.jpg", { gasLimit: 1000000 })).to.be.revertedWith("Enrollment charge should be 0.1 ETH");
+  
+      // check that the user count is still 0
+      expect(await daoUser.userCount()).to.equal(0);
     });
   });
 
   describe('updateUserDetails', function () {
+    beforeEach(async function () {
+
+      // deploy the contract and get owner and user addresses
+      [owner, user] = await ethers.getSigners();
+      const DaoUser = await ethers.getContractFactory('DaoUser');
+      daoUser = await DaoUser.deploy();
+      await daoUser.deployed();
+
+      // add a user to the contract
+      await daoUser.addUserDetails("John", "Doe", "johndoe@example.com", "pic.jpg", { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+    });
+
     it("should update the user's details", async function () {
-      const firstName1 = 'John';
-      const lastName1 = 'Doe';
-      const email1 = 'johndoe@example.com';
-      const pic1 = 'imagepath';
+      
       const firstName2 = 'Jane';
       const lastName2 = 'Doe';
       const email2 = 'janedoe@example.com';
       const pic2 = 'imagepath';
-      await daoUser.addUserDetails(firstName1, lastName1, email1, pic1);
+      // call the function with new input parameters
       await daoUser.updateUserDetails(firstName2, lastName2, email2, pic2);
-      const user = await daoUser.userDetails(await ethers.provider.getSigner(0).getAddress());
+      // check that the user's details were updated correctly
+      const user = await daoUser.userDetails(owner.address);
       expect(user.userAddress).to.equal(await ethers.provider.getSigner(0).getAddress());
       expect(user.firstName).to.equal(firstName2);
       expect(user.lastName).to.equal(lastName2);
       expect(user.email).to.equal(email2);
       expect(user.pic).to.equal(pic2);
+    });
+
+    it("should not update the user's details if they don't exist", async function() {
+      
+      // try to update the details of a user that doesn't exist
+      await expect(daoUser.connect(user).updateUserDetails("Jane", "Doe", "janedoe@example.com", "newpic.jpg")).to.not.be.reverted;
+  
+      // check that the user's details were not updated
+      const currentUser = await daoUser.userDetails(owner.address);
+      expect(currentUser.firstName).to.equal("John");
+      expect(currentUser.lastName).to.equal("Doe");
+      expect(currentUser.email).to.equal("johndoe@example.com");
+      expect(currentUser.pic).to.equal("pic.jpg");
+    });
+  });
+
+  describe('getAllUsers', function () {
+    beforeEach(async function () {
+
+      // deploy the contract and get owner and user addresses
+      [owner, user] = await ethers.getSigners();
+      const DaoUser = await ethers.getContractFactory('DaoUser');
+      daoUser = await DaoUser.deploy();
+      await daoUser.deployed();
+
+      // add a user to the contract
+      await daoUser.addUserDetails("John", "Doe", "johndoe@example.com", "pic.jpg", { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+    });
+
+    it("should return an array of all registered users", async function() {
+      // register a new user
+      await daoUser.connect(user).addUserDetails("Jane", "Doe", "janedoe@example.com", "newpic.jpg", { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+    
+      // get all users
+      const allUsers = await daoUser.getAllUsers();
+    
+      // check that the length of the array matches the number of registered users
+      expect(allUsers.length).to.equal(2);
+    
+      // check that the array contains the expected users
+      expect(allUsers[0].firstName).to.equal("John");
+      expect(allUsers[0].lastName).to.equal("Doe");
+      expect(allUsers[0].email).to.equal("johndoe@example.com");
+      expect(allUsers[0].pic).to.equal("pic.jpg");
+    
+      expect(allUsers[1].firstName).to.equal("Jane");
+      expect(allUsers[1].lastName).to.equal("Doe");
+      expect(allUsers[1].email).to.equal("janedoe@example.com");
+      expect(allUsers[1].pic).to.equal("newpic.jpg");
+    });
+
+    it("should return true if a user with the given address is registered", async function() {
+      // check that the owner's address is registered
+      const isOwnerRegistered = await daoUser.isUserAddressPresent(owner.address);
+      expect(isOwnerRegistered).to.be.true;
+    
+      // check that the user's address is not registered
+      const isUserRegistered = await daoUser.isUserAddressPresent(user.address);
+      expect(isUserRegistered).to.be.false;
     });
   });
 });
--- backend/test/Proposal.js
+++ backend/test/Proposal.js
@@ -5,67 +5,105 @@ const { ethers } = require("hardhat");
 describe("Proposal Contract", function () {
   let daoUser;
   let proposal;
-  let owner, addr1, addr2;
-  // Deploy the contracts before each test
-  beforeEach(async function () {
-    // Deploy DaoUser contract
-    const DaoUser = await ethers.getContractFactory("DaoUser");
-    daoUser = await DaoUser.deploy();
+  describe('createProposal', function () {
+    // Deploy the contracts before each test
+    beforeEach(async function () {
+      [owner, addr1, addr2] = await ethers.getSigners();
+      // Deploy DaoUser contract
+      const DaoUser = await ethers.getContractFactory("DaoUser");
+      daoUser = await DaoUser.deploy();
+      // Deploy Proposal contract
+      const Proposal = await ethers.getContractFactory("Proposal");
+      proposal = await Proposal.deploy(daoUser.address);
+      await proposal.deployed();
+      // add a user to the contract
+      await daoUser.addUserDetails("John", "Doe", "johndoe@example.com", "pic.jpg", { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+    });
 
-    // Deploy Proposal contract
-    const Proposal = await ethers.getContractFactory("Proposal");
-    proposal = await Proposal.deploy(daoUser.address);
-    await proposal.deployed();
+    it("Should create a new proposal", async function () {
+      
+      // Call createProposal function to create a new proposal
+      const title = 'Test Proposal';
+      const description = 'Test proposal description';
+      const uploadDocPath = 'https://test-doc-path';
+      const tx = await proposal.createProposal(title, description, uploadDocPath, { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
 
-    [owner, addr1, addr2] = await ethers.getSigners();
+      // Check the emitted event
+      const receipt = await tx.wait();
+      expect(receipt.events[0].event).to.equal('NewProposalCreated');
+      expect(receipt.events[0].args.proposalId).to.equal(0);
+
+      // Check that the proposal was created successfully
+      const proposals = await proposal.getAllProposals();
+      expect(proposals[0].owner).to.equal(owner.address);
+      expect(proposals[0].title).to.equal(title);
+      expect(proposals[0].description).to.equal(description);
+      expect(proposals[0].uploadDocPath).to.equal(uploadDocPath);
+      expect(proposals[0].status).to.equal(0); // ProposalStatus.Active
+      expect(proposals[0].id).to.equal(0);
+    });
+
+    it('should not allow creation of proposal when another is active', async function () {
+
+      const title = 'Test Proposal';
+      const description = 'Test proposal description';
+      const uploadDocPath = 'https://test-doc-path';
+      await proposal.createProposal(title, description, uploadDocPath, { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+
+      await expect(proposal.createProposal(title, description, uploadDocPath, { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 })).to.be.revertedWith(
+        'Already another Proposal is active'
+      );
+    });
+
+    it('should not allow creation of proposal by unregistered user', async function () {
+      const title = 'Test Proposal';
+      const description = 'Test proposal description';
+      const uploadDocPath = 'https://test-doc-path';
+      await expect(proposal.connect(addr1).createProposal(title, description, uploadDocPath, { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 })).to.be.revertedWith(
+        'User Not registered'
+      );
+    });
   });
+  describe('vote', function () {
+    // Deploy the contracts before each test
+    beforeEach(async function () {
+      [owner, addr1, addr2, addr3] = await ethers.getSigners();
+      // Deploy DaoUser contract
+      const DaoUser = await ethers.getContractFactory("DaoUser");
+      daoUser = await DaoUser.deploy();
+      // Deploy Proposal contract
+      const Proposal = await ethers.getContractFactory("Proposal");
+      proposal = await Proposal.deploy(daoUser.address);
+      await proposal.deployed();
+      // add users to the contract
+      await daoUser.addUserDetails("John", "Doe", "johndoe@example.com", "pic.jpg", { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+      await daoUser.connect(addr1).addUserDetails("Jane", "Doe", "johndoe@example.com", "pic.jpg", { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+      await daoUser.connect(addr2).addUserDetails("Joe", "Doe", "johndoe@example.com", "pic.jpg", { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+      //create a proposal
+      await proposal.createProposal('Test Proposal', 'Test proposal description', 'uploadDocPath', { value: ethers.utils.parseEther("0.1"), gasLimit: 1000000 });
+    });
 
-  it("Should create a new proposal", async function () {
-    //Register user
-    await daoUser.addUserDetails('Test', 'User', 'test@gmail.com', 'pic');
+    it("should allow a registered user to vote on an active proposal", async function() {
+      const proposalId = 0;
+      await proposal.vote(true, proposalId);
+      const votingDetails = await proposal.ProposalVotingDetails(proposalId, 0);
+      expect(votingDetails.user).to.equal(owner.address);
+      expect(votingDetails.vS).to.equal(1); // 1 is the VotingStatus.Accept value
+    });
 
-    // Call createProposal function to create a new proposal
-    const title = 'Test Proposal';
-    const description = 'Test proposal description';
-    const uploadDocPath = 'https://test-doc-path';
-    const tx = await proposal.createProposal(title, description, uploadDocPath);
+    it("should reject votes from unregistered users", async function() {
+      const proposalId = 0;
+      await expect(proposal.connect(addr3).vote(true, proposalId)).to.be.revertedWith("User Not registered");
+    });
 
-    // Check the emitted event
-    const receipt = await tx.wait();
-    expect(receipt.events[0].event).to.equal('NewProposalCreated');
-    expect(receipt.events[0].args.proposalId).to.equal(0);
+    it("should reject votes on invalid proposals", async function() {
+      await expect(proposal.vote(true, 1)).to.be.revertedWith("Invalid Proposal");
+    });
 
-    // Check that the proposal was created successfully
-    const proposals = await proposal.getAllProposals();
-    expect(proposals[0].owner).to.equal(await ethers.provider.getSigner(0).getAddress());
-    expect(proposals[0].title).to.equal(title);
-    expect(proposals[0].description).to.equal(description);
-    expect(proposals[0].uploadDocPath).to.equal(uploadDocPath);
-    expect(proposals[0].status).to.equal(0);
-    expect(proposals[0].id).to.equal(0);
-  });
-
-  it('should not allow creation of proposal when another is active', async function () {
-    //Register user
-    await daoUser.addUserDetails('Test', 'User', 'test@gmail.com', 'pic');
-
-    const title = 'Test Proposal';
-    const description = 'Test proposal description';
-    const uploadDocPath = 'https://test-doc-path';
-    await proposal.createProposal(title, description, uploadDocPath);
-
-    await expect(proposal.createProposal(title, description, uploadDocPath)).to.be.revertedWith(
-      'Already another Proposal is active'
-    );
-  });
-
-  it('should not allow creation of proposal by unregistered user', async function () {
-    const title = 'Test Proposal';
-    const description = 'Test proposal description';
-    const uploadDocPath = 'https://test-doc-path';
-
-    await expect(proposal.createProposal(title, description, uploadDocPath)).to.be.revertedWith(
-      'User Not registered'
-    );
+    it("should reject votes from users who have already voted", async function() {
+      const proposalId = 0;
+      await proposal.vote(true, proposalId);
+      await expect(proposal.vote(false, proposalId)).to.be.revertedWith("You have already voted");
+    });
   });
 });
